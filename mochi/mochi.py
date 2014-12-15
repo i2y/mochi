03280c5d8eb13a22ee59752428df9fe6824852f0
Matthias Bussonnier <bussonniermatthias@gmail.com>
2014-12-15 13:31:52 +0100

    some refactor

--- mochi/mochi.py
+++ mochi/mochi.py
@@ -7,6 +7,7 @@
 
 import ast
 import sys
+import traceback
 from types import FunctionType
 from functools import reduce, partial
 from itertools import chain
@@ -849,6 +850,7 @@ class OutputPort(object):
 
 current_input_port = InputPort()
 current_output_port = OutputPort()
+current_error_port = OutputPort(file=sys.__stderr__)
 py_print = print
 
 
@@ -2999,11 +3001,13 @@ def interact():
                 else:
                     break
             except Exception as e:
-                print(e)
+                py_print(e,file=current_error_port)
                 continuation_flag = False
                 buffer = ''
                 continue
+            eval_tokens(tokens)
 
+def eval_tokens(tokens):
         try:
             sexps = parse(tokens.__iter__())
             for sexp in sexps:
@@ -3016,10 +3020,21 @@ def interact():
                     code = compile(py_ast, '<string>', 'exec')
                     if code is not None:
                         exec(code, global_env)
-                    py_print()
+                    py_print(file=current_output_port)
         except Exception as e:
-            # traceback.print_tb(sys.exc_info()[2])
-            print('*** ERROR: ' + str(e))
+            traceback.print_tb(sys.exc_info()[2], file=current_error_port)
+            py_print('*** ERROR: ' + str(e), file=current_error_port)
+
+def eval_code_block(block):
+    block = block +'\n'
+    tokens = []
+
+    lexer = lex(block, repl_mode=True)
+
+    for last in lexer:
+        tokens.append(last)
+    eval_tokens(tokens)
+
 
 
 py_eval = eval
