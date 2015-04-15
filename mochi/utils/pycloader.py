cc8f2401e57ffd142efc599b64ab617d0d773c27
Mike Mueller <mmueller@python-academy.de>
2015-04-15 22:17:54 +0200

    No class needed. Bind argument `__file__` with `functools.partial`.

--- mochi/utils/pycloader.py
+++ mochi/utils/pycloader.py
@@ -6,28 +6,23 @@ import os
 from mochi.core import pyc_compile_monkeypatch
 
 
-class PycLoader(object):
-    """Loading Mochi functions into Python.
+def get_function(name, file_path):
+    """Python function from Mochi.
+
+    Compiles a Mochi file to Python bytecode and returns the
+    imported function.
     """
+    return getattr(get_module(name, file_path), name)
 
-    def __init__(self, file_path):
-        self.base_path = os.path.dirname(file_path)
 
-    def get_function(self, name):
-        """Python function from Mochi.
+def get_module(name, file_path):
+    """Python function from Mochi.
 
-        Compiles a Mochi file to Python bytecode and returns the
-        imported function.
-        """
-        return getattr(self.get_module(name), name)
-
-    def get_module(self, name):
-        """Python function from Mochi.
-
-        Compiles a Mochi file to Python bytecode and returns the
-        Python module.
-        """
-        mochi_name = os.path.join(self.base_path, name + '.mochi')
-        py_name = os.path.join(self.base_path, name + '.pyc')
-        pyc_compile_monkeypatch(mochi_name, py_name)
-        return __import__(name)
+    Compiles a Mochi file to Python bytecode and returns the
+    Python module.
+    """
+    base_path = os.path.dirname(file_path)
+    mochi_name = os.path.join(base_path, name + '.mochi')
+    py_name = os.path.join(base_path, name + '.pyc')
+    pyc_compile_monkeypatch(mochi_name, py_name)
+    return __import__(name)
