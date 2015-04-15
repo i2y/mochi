1f16b7302b75eaeab468bbdab7b24019119c38f4
Mike Mueller <mmueller@python-academy.de>
2015-04-15 21:37:12 +0200

    More test. New module loader allows loading of modules, not only functions.

--- mochi/utils/pycloader.py
+++ mochi/utils/pycloader.py
@@ -13,14 +13,21 @@ class PycLoader(object):
     def __init__(self, file_path):
         self.base_path = os.path.dirname(file_path)
 
-
     def get_function(self, name):
         """Python function from Mochi.
 
         Compiles a Mochi file to Python bytecode and returns the
         imported function.
         """
+        return getattr(self.get_module(name), name)
+
+    def get_module(self, name):
+        """Python function from Mochi.
+
+        Compiles a Mochi file to Python bytecode and returns the
+        Python module.
+        """
         mochi_name = os.path.join(self.base_path, name + '.mochi')
-        py_name = os.path.join(self.base_path, name + '_comp.pyc')
+        py_name = os.path.join(self.base_path, name + '.pyc')
         pyc_compile_monkeypatch(mochi_name, py_name)
-        return getattr(__import__(name), name)
+        return __import__(name)
--- tests/test_patternmatching.py
+++ tests/test_patternmatching.py
@@ -1,15 +1,36 @@
 import os
 
+from pyrsistent import pvector
 from mochi.utils.pycloader import PycLoader
 
 loader = PycLoader(__file__)
 factorial = loader.get_function('factorial')
+fizzbuzz = loader.get_function('fizzbuzz')
+patterns = loader.get_module('patterns')
+
 
 def test_factorial_1():
     assert factorial(1) == 1
+
 
 def test_factorial_2():
     assert factorial(2) == 2
 
 
+def test_fizzbuzz():
+    res = [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz',
+           13, 14, 'fizzbuzz', 16, 17, 'fizz', 19, 'buzz', 'fizz', 22, 23,
+           'fizz', 'buzz', 26, 'fizz', 28, 29, 'fizzbuzz']
+    assert [fizzbuzz(n) for n in range(1, 31)] == res
 
+
+def test_list_seq():
+    assert patterns.list_seq() == 3
+
+
+def test_match_rest():
+    assert patterns.match_rest() == pvector([2, 3])
+
+
+def test_list_seq_auto():
+    assert patterns.list_seq() == patterns.result_list_seq()
